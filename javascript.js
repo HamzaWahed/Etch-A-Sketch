f66baef2c25f9fdc680170d63b619e999a3a6a84
Hamza Wahed <hamzawahed@dal.ca>
2022-07-03 11:45:08 -0300

    add functions to create a new grid

--- javascript.js
+++ javascript.js
@@ -1,10 +1,13 @@
-function createGrid(){
+function createGrid(amount){
     const grid = document.querySelector(".grid");
-    for(let i=0;i<256;i++){
+    for(let i=0;i<amount;i++){
         let newDiv = document.createElement("div");
         newDiv.classList.add("square");
         grid.appendChild(newDiv)
     }
+    
+    changeStyle(Math.sqrt(amount));
+    changeColor();
 }
 
 function changeColor(){
@@ -13,8 +16,32 @@ function changeColor(){
         element.addEventListener("mouseenter",function(){
             element.style.backgroundColor = 'blue';
         })
-        );
+    );
 }
 
-createGrid();
-changeColor();
+function createNewGrid(){
+    let button = document.querySelector(".new");
+    button.addEventListener("click",function(){
+        let amount = prompt("How many squares per side? (100 max)");
+        while(amount > 100 || amount < 0){
+            amount = prompt("Enter a valid number!")
+        }
+
+        remove();
+        createGrid(amount*amount);
+    });
+}
+
+function remove(){
+    const box = document.querySelectorAll(".square");
+    box.forEach(element => element.remove());
+}
+
+function changeStyle(amount){
+    let grid = document.querySelector(".grid");
+    grid.style.gridTemplateColumns = `repeat(${amount}, 30px)`;
+    grid.style.gridTemplateRows = `repeat(${amount}, 30px)`;
+}
+
+createGrid(256);
+createNewGrid();
