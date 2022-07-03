e45efdf11a5f8f041ceee46ffbea4ddbe71313d7
Hamza Wahed <hamzawahed@dal.ca>
2022-07-03 11:20:32 -0300

    add function to change color of div on hover

--- javascript.js
+++ javascript.js
@@ -2,7 +2,19 @@ function createGrid(){
     const grid = document.querySelector(".grid");
     for(let i=0;i<256;i++){
         let newDiv = document.createElement("div");
+        newDiv.classList.add("square");
         grid.appendChild(newDiv)
     }
 }
 
+function changeColor(){
+    let box = document.querySelectorAll(".square")
+    box.forEach(element => 
+        element.addEventListener("mouseenter",function(){
+            element.style.backgroundColor = 'blue';
+        })
+        );
+}
+
+createGrid();
+changeColor();
